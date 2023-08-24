8829ba73a264275a247f8ffadc265bdbf4d378df
jones <35585020+fleeced@users.noreply.github.com>
2023-08-24 12:14:43 -0600

    integrate transaction model and CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connectDB = require('./db');
 const User = require('./models/user');
+const Transaction = require('./models/transaction');
 
 const typeDefs = gql`
   type Query {
     hello: String
+    transactions(userId: ID!): [Transaction]
   }
 
   type Mutation {
@@ -18,12 +20,29 @@ const typeDefs = gql`
     userId: ID!
     token: String!
   }
+
+  type Transaction {
+    id: ID!
+    description: String!
+    amount: Float!
+    date: String!
+    type: String!
+}
+
+type Mutation {
+    createTransaction(userId: ID!, description: String!, amount: Float!, type: String!): Transaction
+    updateTransaction(id: ID!, description: String, amount: Float, type: String): Transaction
+    deleteTransaction(id: ID!): ID
+}
 `;
 
 
 const resolvers = {
     Query: {
-        hello: () => "Hello, Neo Wallet!"
+        hello: () => "Hello, Neo Wallet!",
+        transactions: async (_, { userId }) => { 
+            return await Transaction.find({ userId });
+        }
     },
     Mutation: {
         signUp: async (_, { username, email, password }) => {
@@ -60,6 +79,41 @@ const resolvers = {
             const token = jwt.sign({ userId: user.id }, 'YOUR_SECRET_KEY', { expiresIn: '1h' });
 
             return { userId: user.id, token };
+        },
+        createTransaction: async (_, { userId, description, amount, type }) => {
+            const transaction = new Transaction({
+                userId,
+                description,
+                amount,
+                type
+            });
+
+            await transaction.save();
+            return transaction;
+        },
+
+        updateTransaction: async (_, { id, description, amount, type }) => {
+            const transaction = await Transaction.findById(id);
+            if (!transaction) {
+                throw new Error('Transaction not found');
+            }
+
+            if (description) transaction.description = description;
+            if (amount) transaction.amount = amount;
+            if (type) transaction.type = type;
+
+            await transaction.save();
+            return transaction;
+        },
+
+        deleteTransaction: async (_, { id }) => {
+            const transaction = await Transaction.findById(id);
+            if (!transaction) {
+                throw new Error('Transaction not found');
+            }
+
+            await transaction.remove();
+            return id;
         }
     }
 };
