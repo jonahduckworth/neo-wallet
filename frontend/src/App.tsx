5da583ed2719a5cc5aa9e2808f2a097d56481b27
jones <35585020+fleeced@users.noreply.github.com>
2023-08-24 15:14:22 -0600

    added functional logout button when logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,30 @@ import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
   const [userId, setUserId] = useState<string | null>(null);
 
   const handleLogin = (id: string) => {
+    console.log("Logged in as:", id);
     setIsLoggedIn(true);
     setUserId(id);
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUserId(null);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
     <Router>
       <div className="App">
         <AppRoutes isLoggedIn={isLoggedIn} userId={userId} handleLogin={handleLogin} />
+        {isLoggedIn && <LogoutButton handleLogout={handleLogout} />}
       </div>
     </Router>
   );
