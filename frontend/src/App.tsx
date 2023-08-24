c117b0fdffba9c6c770e10ff7198e496e59b7eee
jones <35585020+fleeced@users.noreply.github.com>
2023-08-24 14:37:37 -0600

    add and use react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AuthPage from './components/AuthPage';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState<string | null>(null);
+
+  const handleLogin = (id: string) => {
+    setIsLoggedIn(true);
+    setUserId(id);
+  };
+
   return (
-    <div className="App">
-      <AuthPage />
-    </div>
+    <Router>
+      <div className="App">
+      <Routes>
+        <Route path="/login" element={isLoggedIn ? <Navigate to="/dashboard" /> : <AuthPage onLogin={handleLogin} />} />
+        <Route path="/dashboard" element={isLoggedIn ? <Dashboard userId={userId!} /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
