1010a855b3fea839b97a4fdafc7ac3287c2b3e42
jones <35585020+fleeced@users.noreply.github.com>
2023-08-27 13:30:20 -0600

    added transaction types

---
+++ frontend/src/components/Transactions/Types/index.ts
@@ -0,0 +1,25 @@
+interface Transaction {
+    id: string;
+    description: string;
+    amount: number;
+    date: string;
+    type: string;
+  }
+  
+interface TransactionProps {
+  userId: string;
+}
+
+interface TransactionFormProps {
+  formData: TransactionFormData;
+  onSubmit: (event: React.FormEvent) => void;
+  onInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void;
+}
+
+interface TransactionFormData {
+  description: string;
+  amount: string;
+  type: 'income' | 'expense';
+}
+
+export type { Transaction, TransactionProps, TransactionFormProps, TransactionFormData };
